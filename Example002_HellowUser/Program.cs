67b4aa755a6633da356f2586f3912f873122791e
Shiyko D. <niipmt-danil@hotmail.com>
2023-11-03 01:58:48 +0300

    add 2

--- Example002_HellowUser/Program.cs
+++ Example002_HellowUser/Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.Write("Введите ваше имя");
+Console.Write("Введите ваше имя 2");
 string username =  Console.ReadLine();
 Console.Write("Привет, ");
 Console.Write(username);
