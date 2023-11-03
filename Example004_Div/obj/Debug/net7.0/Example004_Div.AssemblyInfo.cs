450ec4a4ce1c9ce8f6f39548e204c040ed137b2c
Shiyko D. <niipmt-danil@hotmail.com>
2023-11-04 01:44:06 +0300

    Example004_Div

--- Example004_Div/obj/Debug/net7.0/Example004_Div.AssemblyInfo.cs
+++ Example004_Div/obj/Debug/net7.0/Example004_Div.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Example003_Sum")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Example004_Div")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Example003_Sum")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Example003_Sum")]
+[assembly: System.Reflection.AssemblyProductAttribute("Example004_Div")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Example004_Div")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
