6bc497e53ffa024434c45508c0da09aeca4470f4
Shiyko D. <niipmt-danil@hotmail.com>
2023-11-12 00:24:31 +0300

    add new line

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Как тебя зовут?");
+string username = Console.ReadLine();
+
+
+if (username.ToLower() == "danil")
+{
+    Console.WriteLine($"Привет, {username}! Как поживаешь?");
+}
+else 
+{ 
+    Console.WriteLine("Я тебя не знаю!"); 
+}
+
+
+Console.ReadLine();
